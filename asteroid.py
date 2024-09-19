f291e59b7af88ab424ba721e574364cb6ab21992
root <kasper.jarlgren@gmail.com>
2024-09-19 10:38:29 +0200

    asteroids

---
+++ asteroid.py
@@ -0,0 +1,17 @@
+from circleshape import *
+from constants import *
+
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+        self.x = x
+        self.y = y
+        self.radius = radius
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, 'white', self.position, self.radius, width=2)
+
+    def update(self, dt):
+        self.position += dt * self.velocity
+
--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 import pygame
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import *
 
 def main():
     pygame.init()
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    
+    drawable = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+    
+
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    asteroidfield = AsteroidField()
 
     while True:
         for event in pygame.event.get():
@@ -12,8 +31,15 @@ def main():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_q:
                     return
+        
         screen.fill('black')
+        for obj in drawable:
+             obj.draw(screen)
+        for obj in updatable:
+             obj.update(dt)
+            
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
-    main()
+        main()
